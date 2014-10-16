138b776aebae30be90ea6e0eb3a9e1cf4ab0fb7d
thiagofrohlich <thi.frohlich@gmail.com>
2014-10-16 20:39:41 -0300

    Subir serviço de teste
    HelloRest (http://localhost:8080/tcc-ufpr-web/greeting) funcionando

---
+++ tcc-ufpr-web/src/main/java/br/ufpr/rest/HelloRest.java
@@ -0,0 +1,45 @@
+package br.ufpr.rest;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloRest {
+
+	private final static String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping("/greeting")
+	public HttpEntity<Greeting> greeting(
+			@RequestParam(value="name", required=false, defaultValue="World")
+			String name) {
+		
+		return new HttpEntity<Greeting>(new Greeting(counter.incrementAndGet(), String.format(template, name)));
+	}
+	
+	public class Greeting {
+		
+		private final long id;
+		private final String content;
+		
+		public Greeting(long id, String content) {
+			this.id = id;
+			this.content = content;
+		}
+
+		public long getId() {
+			return id;
+		}
+
+		public String getContent() {
+			return content;
+		}
+		
+	}
+	
+	
+}
